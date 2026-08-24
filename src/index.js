8b2b69d5cae92f6ed4b5dffaadd36df8720fc370
simonfab18 <simognf@gmail.com>
2026-08-24 18:58:36 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Welcome to Livesports API' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
