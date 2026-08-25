a6a98bfe417eecd89f8ccbe8fc82bfdaaae30eae
simonfab18 <simognf@gmail.com>
2026-08-25 21:40:22 +0800

    implement the web socket server

--- src/validation/matches.js
+++ src/validation/matches.js
@@ -19,8 +19,8 @@ export const createMatchSchema = z
     sport: z.string().min(1),
     homeTeam: z.string().min(1),
     awayTeam: z.string().min(1),
-    startTime: z.iso.datetime,
-    endTime: z.iso.datetime,
+    startTime: z.iso.datetime(),
+    endTime: z.iso.datetime(),
     homeScore: z.coerce.number().int().nonnegative().optional(),
     awayScore: z.coerce.number().int().nonnegative().optional(),
   })
