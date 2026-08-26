58fab51385a7efb9a15885c6b8656aa092655bf3
simonfab18 <simognf@gmail.com>
2026-08-26 18:09:33 +0800

    implement arcjet security

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import express from 'express';
+import http from 'http';
 import {matchRouter} from "./routes/matches.js";
+import {attachWebSocketServer} from "./ws/server.js";
+import {securityMiddleware} from "../arcjet.js";
+
+const PORT = Number(process.env.PORT) || 8000;
+const HOST = process.env.HOST || 8000;
 
 const app = express();
-const PORT = 8000;
+const server = http.createServer(app);
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Welcome to Livesports API' });
+  res.send('Hello from Express server!' );
 });
 
-app.use('/matches', matchRouter)
+app.use(securityMiddleware());
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.use('/matches', matchRouter);
+
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+
+server.listen(PORT, HOST,() => {
+  const baseURl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+
+  console.log(`Server running on ${ baseURl }`);
+  console.log(`WebSocket Server is running on ${baseURl.replace('http', 'ws')}/ws`);
 });
---
+++ src/ws/server.js
@@ -0,0 +1,64 @@
+import {WebSocket, WebSocketServer} from 'ws'
+import {wsArcjet} from "../../arcjet.js";
+
+function sendJson(socket, payload){
+    if(socket.readyState !== WebSocket.OPEN) return;
+
+    socket.send(JSON.stringify(payload));
+}
+
+function broadcast(wss, payload){
+    for (const client of wss.clients) {
+        if(client.readyState !== WebSocket.OPEN) continue;
+
+        client.send(JSON.stringify(payload));
+    }
+}
+
+export function attachWebSocketServer(server) {
+    const wss = new WebSocketServer({ server, path: '/ws', maxPayload: 1024 * 1024 });
+
+    wss.on('connection', async (socket, req) => {
+        if(wsArcjet) {
+            try {
+                const decision = await wsArcjet.protect(req)
+
+                if(decision.isDenied()){
+                    const code = decision.reason.isRateLimit() ? 1013 : 1008;
+                    const reason = decision.reason.isRateLimit() ? 'Rate Limit exceeded' : 'Access Denied';
+
+                    socket.close(code, reason);
+                    return;
+                }
+            } catch (e) {
+                console.error('WS connection error', e);
+                socket.close(1011, 'Server Security Error');
+                return;
+            }
+        }
+
+
+        socket.isAlive = true;
+        socket.on('pong', () => {socket.isAlive = true; });
+
+        sendJson(socket, { type: 'welcome' });
+
+        socket.on('error', console.error);
+    });
+
+    const interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            if (ws.isAlive === false) return ws.terminate();
+            ws.isAlive = false;
+            ws.ping();
+        });
+    }, 30000);
+
+    wss.on('close', () => clearInterval(interval));
+
+    function broadcastMatchCreated(match) {
+        broadcast(wss, {type: 'match_created', data: match});
+    }
+    return { broadcastMatchCreated }
+}
+
